00df531a795d38f69a6cb9f496b633f15803fba9
Daniil Halushka <popolzen22@gmail.com>
2024-06-19 15:20:30 +0300

    Fix TopBar

---
+++ app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/CustomTopBar.kt
@@ -0,0 +1,79 @@
+package com.daniil.halushka.todoapp.presentation.screens.elements
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.daniil.halushka.todoapp.R
+import com.daniil.halushka.todoapp.data.repository.TodoRepository
+import com.daniil.halushka.todoapp.ui.theme.TodoAppTheme
+
+@Composable
+fun CustomTopBar(
+    completedItemsCount: Int,
+) {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary),
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+                .height(64.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.primary),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(R.string.my_tasks_todo_s),
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Text(
+                        text = stringResource(
+                            id = R.string.completed_todo_s,
+                            completedItemsCount
+                        ),
+                        fontSize = 20.sp,
+                        color = MaterialTheme.colorScheme.onTertiary
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun Preview1() {
+    TodoAppTheme {
+        Column {
+            CustomTopBar(completedItemsCount = 5)
+            ContainerWithTodo(repository = TodoRepository()) {}
+        }
+    }
+}
--- app/src/main/java/com/daniil/halushka/todoapp/ui/theme/Color.kt
+++ app/src/main/java/com/daniil/halushka/todoapp/ui/theme/Color.kt
@@ -41,12 +41,3 @@ val nightLabelDisableColor = Color(0x26FFFFFF)
 val nightBackPrimaryColor = Color(0xFF161618)
 val nightBackSecondaryColor = Color(0xFF252528)
 val nightBackElevatedColor = Color(0xFF3C3C3F)
-
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
