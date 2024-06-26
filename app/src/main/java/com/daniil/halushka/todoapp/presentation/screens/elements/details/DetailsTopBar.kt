2123463bc305004dae629c0e23f625ec7c3bc32b
Daniil Halushka <popolzen22@gmail.com>
2024-06-26 14:56:43 +0300

    Delete unused Box in DetailsTopBar

--- app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/details/DetailsTopBar.kt
+++ app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/details/DetailsTopBar.kt
@@ -2,7 +2,6 @@ package com.daniil.halushka.todoapp.presentation.screens.elements.details
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.daniil.halushka.todoapp.R
@@ -24,30 +24,29 @@ import com.daniil.halushka.todoapp.ui.theme.AppTheme
 fun DetailsTopBar(
     clickOnNavigationItem: () -> Unit = {}
 ) {
-    Box(
-        modifier = Modifier.padding(16.dp),
-        contentAlignment = Alignment.TopCenter
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
-            Icon(
-                modifier = Modifier
-                    .size(24.dp)
-                    .clickable(onClick = clickOnNavigationItem),
-                imageVector = Icons.Default.Close,
-                contentDescription = stringResource(R.string.close_details_screen),
-                tint = AppTheme.colorScheme.labelPrimaryColor
-            )
-            Text(
-                modifier = Modifier
-                    .clickable(onClick = clickOnNavigationItem),
-                text = stringResource(R.string.save_todo),
-                fontSize = 20.sp,
-                color = AppTheme.colorScheme.blueColor
-            )
-        }
+        Icon(
+            modifier = Modifier
+                .size(24.dp)
+                .clickable(onClick = clickOnNavigationItem),
+            imageVector = Icons.Default.Close,
+            contentDescription = stringResource(R.string.close_details_screen),
+            tint = AppTheme.colorScheme.labelPrimaryColor
+        )
+        Text(
+            modifier = Modifier
+                .clickable(onClick = clickOnNavigationItem),
+            text = stringResource(R.string.save_todo),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = AppTheme.colorScheme.blueColor
+        )
+
     }
 }
