52623a283fafdf5b84176f183a4e86807b88f026
Daniil Halushka <popolzen22@gmail.com>
2024-06-28 00:58:23 +0300

    Add preview to DetailsExpandedDropdown

--- app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/details/DetailsExpandedDropdown.kt
+++ app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/details/DetailsExpandedDropdown.kt
@@ -1,7 +1,9 @@
 package com.daniil.halushka.todoapp.presentation.screens.elements.details
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,17 +11,23 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.daniil.halushka.todoapp.constants.Priority
 import com.daniil.halushka.todoapp.ui.theme.AppTheme
+import com.daniil.halushka.todoapp.ui.theme.TodoAppTheme
 
 @Composable
 fun DetailsExpandedDropdown(
@@ -84,4 +92,51 @@ fun PriorityItemInDropdown(
             onPrioritySelect(priority)
         }
     )
+}
+
+//Turn on interactive mode
+@Composable
+@Preview(name = "Light version", showBackground = true)
+fun DetailsExpandedDropdownPreview() {
+    TodoAppTheme {
+        var dropdownClick: Boolean by remember { mutableStateOf(false) }
+        var selectedPriority by remember { mutableStateOf(Priority.USUAL_PRIORITY) }
+        Row(
+            modifier = Modifier
+                .background(AppTheme.colorScheme.backPrimaryColor)
+        ) {
+            DetailsCollapsedDropdown(
+                priority = selectedPriority,
+                isClicked = { click -> dropdownClick = click }
+            )
+            DetailsExpandedDropdown(
+                expanded = dropdownClick,
+                clickToExpand = { click -> dropdownClick = click },
+                onPrioritySelect = { newPriority -> selectedPriority = newPriority }
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(name = "Dark version", uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun DetailsExpandedDropdownPreviewDark() {
+    TodoAppTheme {
+        var dropdownClick: Boolean by remember { mutableStateOf(false) }
+        var selectedPriority by remember { mutableStateOf(Priority.USUAL_PRIORITY) }
+        Row(
+            modifier = Modifier
+                .background(AppTheme.colorScheme.backPrimaryColor)
+        ) {
+            DetailsCollapsedDropdown(
+                priority = selectedPriority,
+                isClicked = { click -> dropdownClick = click }
+            )
+            DetailsExpandedDropdown(
+                expanded = dropdownClick,
+                clickToExpand = { click -> dropdownClick = click },
+                onPrioritySelect = { newPriority -> selectedPriority = newPriority }
+            )
+        }
+    }
 }
