dceaa91f210eeec21428c5665a10914f720e8018
Daniil Halushka <popolzen22@gmail.com>
2024-06-28 00:30:57 +0300

    Add preview to FAB

--- app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/home/HomeCustomFAB.kt
+++ app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/home/HomeCustomFAB.kt
@@ -1,5 +1,6 @@
 package com.daniil.halushka.todoapp.presentation.screens.elements.home
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -9,9 +10,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.daniil.halushka.todoapp.ui.theme.AppTheme
+import com.daniil.halushka.todoapp.ui.theme.TodoAppTheme
 
 @Composable
 fun CustomFAB(
@@ -33,5 +37,23 @@ fun CustomFAB(
             imageVector = Icons.Default.Add,
             contentDescription = null,
         )
+    }
+}
+
+@Composable
+@Preview(name = "Light version", showBackground = true)
+fun FABPreview() {
+    TodoAppTheme {
+        val navigationController = rememberNavController()
+        CustomFAB(navigationController = navigationController)
+    }
+}
+
+@Composable
+@Preview(name = "Dark version", uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun FABPreviewDark() {
+    TodoAppTheme {
+        val navigationController = rememberNavController()
+        CustomFAB(navigationController = navigationController)
     }
 }
