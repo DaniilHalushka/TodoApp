658776ad1b522bcef5af4ff38b27dff71d3b6ec6
Daniil Halushka <popolzen22@gmail.com>
2024-06-28 00:39:04 +0300

    Add preview to DetailsCollapsedDropdown

--- app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/details/DetailsCollapsedDropdown.kt
+++ app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/details/DetailsCollapsedDropdown.kt
@@ -1,5 +1,6 @@
 package com.daniil.halushka.todoapp.presentation.screens.elements.details
 
+import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,9 +9,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.daniil.halushka.todoapp.R
+import com.daniil.halushka.todoapp.constants.Priority
 import com.daniil.halushka.todoapp.ui.theme.AppTheme
+import com.daniil.halushka.todoapp.ui.theme.TodoAppTheme
 
 @Composable
 fun DetailsCollapsedDropdown(
@@ -32,6 +36,28 @@ fun DetailsCollapsedDropdown(
             text = priority,
             color = AppTheme.colorScheme.labelTertiaryColor,
             style = AppTheme.typographyScheme.bodyText
+        )
+    }
+}
+
+@Composable
+@Preview(name = "Light version", showBackground = true)
+fun DetailsCollapsedDropdownPreview() {
+    TodoAppTheme {
+        DetailsCollapsedDropdown(
+            priority = Priority.USUAL_PRIORITY,
+            isClicked = {}
+        )
+    }
+}
+
+@Composable
+@Preview(name = "Dark version", uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun DetailsCollapsedDropdownDark() {
+    TodoAppTheme {
+        DetailsCollapsedDropdown(
+            priority = Priority.USUAL_PRIORITY,
+            isClicked = {}
         )
     }
 }
