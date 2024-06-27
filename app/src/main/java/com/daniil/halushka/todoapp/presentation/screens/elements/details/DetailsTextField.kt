9479f07fbcf24310a48e480c99c8f9232a519ad1
Daniil Halushka <popolzen22@gmail.com>
2024-06-28 01:04:10 +0300

    Add preview to DetailsTextField

--- app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/details/DetailsTextField.kt
+++ app/src/main/java/com/daniil/halushka/todoapp/presentation/screens/elements/details/DetailsTextField.kt
@@ -1,5 +1,6 @@
 package com.daniil.halushka.todoapp.presentation.screens.elements.details
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -11,13 +12,19 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.daniil.halushka.todoapp.R
 import com.daniil.halushka.todoapp.ui.theme.AppTheme
+import com.daniil.halushka.todoapp.ui.theme.TodoAppTheme
 
 @Composable
 fun DetailsTextField(
@@ -49,7 +56,6 @@ fun DetailsTextField(
                 unfocusedIndicatorColor = AppTheme.colorScheme.backPrimaryColor,
                 focusedContainerColor = AppTheme.colorScheme.backSecondaryColor,
                 unfocusedContainerColor = AppTheme.colorScheme.backSecondaryColor,
-
                 )
         )
     }
@@ -61,5 +67,27 @@ fun TextFieldPlaceholder() {
         text = stringResource(R.string.what_you_need_to_do),
         color = AppTheme.colorScheme.labelTertiaryColor,
         fontSize = 16.sp
+    )
+}
+
+@Composable
+@Preview(name = "Light version", showBackground = true)
+fun DetailsTextFieldPreview() {
+    TodoAppTheme {
+        var text by remember { mutableStateOf( "") }
+        DetailsTextField(
+            text = "Text text to preview",
+            onTextChange = { newText -> text = newText }
+        )
+    }
+}
+
+@Composable
+@Preview(name = "Dark version", uiMode = Configuration.UI_MODE_NIGHT_YES)
+fun DetailsTextFieldDark() {
+    var text by remember { mutableStateOf( "") }
+    DetailsTextField(
+        text = "Text text to preview",
+        onTextChange = { newText -> text = newText }
     )
 }
