5498a14da5800cc28d70df6c293f8cea398dab60
Daniil Halushka <popolzen22@gmail.com>
2024-07-11 15:33:01 +0300

    Add extension to convert todoItemDatabase to todoItem

--- app/src/main/java/com/daniil/halushka/todoapp/data/database/model/TodoItemDatabase.kt
+++ app/src/main/java/com/daniil/halushka/todoapp/data/database/model/TodoItemDatabase.kt
@@ -2,6 +2,7 @@ package com.daniil.halushka.todoapp.data.database.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.daniil.halushka.todoapp.data.models.TodoItem
 
 @Entity(tableName = "todo_items")
 data class TodoItemDatabase(
@@ -13,3 +14,17 @@ data class TodoItemDatabase(
     val deadline: Long? = null,
     val changeDate: Long? = null
 )
+
+fun TodoItemDatabase.toTodoItem(): TodoItem {
+    val (id, text, priority, isDone, startDate, deadline, changeDate) = this
+
+    return TodoItem(
+        id = id,
+        text = text,
+        priority = priority,
+        isDone = isDone,
+        startDate = startDate,
+        deadline = deadline,
+        changeDate = changeDate
+    )
+}
